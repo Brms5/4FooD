4ea5d4aaf7d27b4e914c4aee7feaf8ccd571bb09
bruno.moreira <bruno.moreira@meta.com.br>
2022-03-19 10:38:35 -0300

    feat: system and route structure

---
+++ 4food/src/pages/signup-address/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SignUpAddressPage() {
+  return (
+    <div>
+      <h1>Página de cadastro de endereço</h1>
+    </div>
+  );
+}
+
+export default SignUpAddressPage;
---
+++ 4food/src/pages/user/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function UserEditionPage() {
+  return (
+    <div>
+      <h1>Página de Edição de usuários</h1>
+    </div>
+  );
+}
+
+export default UserEditionPage;
